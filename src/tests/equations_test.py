953cb2bc36f29ef03c93c812b9c67e01a2a724c8
Nikita Essine <nikita.essine@helsinki.fi>
2022-12-06 12:26:46 +0200

    olion palautusarvo testattu

--- src/tests/equations_test.py
+++ src/tests/equations_test.py
@@ -8,6 +8,7 @@ class TestEquations(unittest.TestCase):
         self.equations = Equations()
         self.equations.num1 = 6
         self.equations.num2 = 7
+        self.equations.operat = "+"
 
     def test_plus_works(self):
 
@@ -15,3 +16,6 @@ class TestEquations(unittest.TestCase):
 
     def test_minus_works(self):
         self.assertEqual(self.equations.minus(), -1)
+
+    def test_return_value(self):
+          self.assertEqual(str(self.equations), 'Paljonko on 6 + 7?')
